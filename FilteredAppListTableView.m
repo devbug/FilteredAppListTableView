6cad91b765a2355cd82a45e02c6605ddec61e1ce
deVbug <devbug@devbug.me>
2013-10-17 22:45:45 +0900

    fix korean index string

--- FilteredAppListTableView.m
+++ FilteredAppListTableView.m
@@ -160,7 +160,7 @@ Class newHudClass;
 	
 	// http://pastebin.com/7YkT4dbk
 	// 한글 로마자 변환 프로그램 by 동성
-	NSString *choCharset = @"ㄱㄲㄴㄷㄸㄹㅁㅂㅃᄉㅆㅇㅈㅉㅊㅋㅌㅍㅎ";
+	NSString *choCharset = @"ㄱㄲㄴㄷㄸㄹㅁㅂㅃᄉㅆᄋㅈㅉㅊㅋㅌㅍㅎ";
 	
 	unichar header = ' ', temp;
 	for (NSString *displayId in sortedArray) {
